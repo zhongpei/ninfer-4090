506e6aed2eeea1c8f76b29d3208994fb47285efa
fofo <qqqeee@gmail.com>
2026-09-24 21:11:18 +0800

    feat(yarn): carry scaling policy in sequence plan

--- src/models/qwen3_5/program/planning/startup.h
+++ src/models/qwen3_5/program/planning/startup.h
@@ -91,6 +91,8 @@ struct SequencePlanningInputs {
     models::LoadOptions features;
     bool use_cuda_graph = true;
     bool causal_scoring = false;
+    float rope_scaling_factor                   = 1.0F;
+    std::uint32_t rope_scaling_original_context = 262144;
     int device          = 0;
     ContextCacheOptions context_cache;
 };
@@ -114,6 +116,8 @@ struct SequencePlanImpl {
     models::LoadOptions features;
     bool use_cuda_graph = true;
     bool causal_scoring = false;
+    float rope_scaling_factor                   = 1.0F;
+    std::uint32_t rope_scaling_original_context = 262144;
     int device          = 0;
     ContextCacheOptions context_cache;
     PersistentLayout persistent;
